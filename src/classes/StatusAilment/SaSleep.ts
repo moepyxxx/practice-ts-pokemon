70781eeb60aa6c5dbfb3bebd3275c0463f96d6de
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-01-27 08:29:11 +0900

    やけど・どく・もうどく・ねむり・こおりクラス追加

---
+++ src/classes/StatusAilment/SaSleep.ts
@@ -0,0 +1,30 @@
+import { StatusAilment } from '../StatusAilment';
+
+export class SaSleep extends StatusAilment {
+
+  /**
+   * 名前
+   */
+  protected _name: string = 'ねむり';
+
+  /**
+   * 状態異常になった時のメッセージ
+   */
+  protected _sickedMessage: string = 'ねむってしまった';
+
+  /**
+   * すでに状態異常だったときのメッセージ
+   */
+  protected _alreadySickedMessage: string = 'すでにねむっている';
+
+  /**
+   * 状態異常のままターンになったときのメッセージ
+   */
+  protected _sickedTurnMessage: string = 'ぐうぐうねむっている';
+
+  /**
+   * 状態異常が回復したときのメッセージ
+   */
+  protected _sickedRecoceryMessage = 'めをさました';
+
+}
--- src/utils/datas/statusAilmentDatas.ts
+++ src/utils/datas/statusAilmentDatas.ts
@@ -1,13 +1,19 @@
-import { SaParalysis } from '../../classes/StatusAilment/SaParalysis';
 import { StatusAilment } from '../../classes/StatusAilment';
+import { SaParalysis } from '../../classes/StatusAilment/SaParalysis';
+import { SaBadPoison } from '../../classes/StatusAilment/SaBadPoison';
+import { SaPoison } from '../../classes/StatusAilment/SaPoison';
+import { SaFreeze } from '../../classes/StatusAilment/SaFreeze';
+import { SaSleep } from '../../classes/StatusAilment/SaSleep';
+import { SaBurn } from '../../classes/StatusAilment/SaBurn';
 
 export const STATUS_AILMENT_CLASS_LIST: {
   [key: string]: StatusAilment;
 } = {
-  'saBurn': new SaParalysis(),
-  'saFreeze': new SaParalysis(),
+  'saBurn': new SaBurn(),
+  'saFreeze': new SaFreeze(),
   'saParalysis': new SaParalysis(),
-  'saPoison': new SaParalysis(),
-  'saSleep': new SaParalysis(),
+  'saPoison': new SaPoison(),
+  'saBadPoison': new SaBadPoison(),
+  'saSleep': new SaSleep(),
   'saFainting': new SaParalysis()
 }
