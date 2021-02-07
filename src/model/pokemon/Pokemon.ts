bc265bcedf17fc778ede4b94515fe5964fc980e6
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-02-08 00:17:02 +0900

    不要なポケモンクラスの要素を消去

--- src/model/pokemon/Pokemon.ts
+++ src/model/pokemon/Pokemon.ts
@@ -1,4 +1,3 @@
-import { TBasicStatus } from "../../utils/type/TBasicStatus";
 import { Group } from '../group/Group';
 import { Move } from '../move/Move';
 
@@ -28,116 +27,21 @@ export abstract class Pokemon {
   }[];
 
   /**
-   * 種族値
-   */
-  protected readonly abstract _basicPokemonStatus: TBasicStatus;
-
-  /**
-   * 個体値
-   */
-  protected abstract _basicIndividualStatus: TBasicStatus = {
-    hp: 0,
-    attack: 0,
-    protected: 0,
-    SPattack: 0,
-    SPprotected: 0,
-    rapidity: 0
-  }
-
-  /**
-   * 努力値
-   */
-  protected abstract _basicEffortStatus: TBasicStatus = {
-    hp: 0,
-    attack: 0,
-    protected: 0,
-    SPattack: 0,
-    SPprotected: 0,
-    rapidity: 0
-  }
-
-  /* レベル・わざ・ステータスは、ExceptPokemonとOwnPokemonのインスタンス生成時に必ず
-
-  /**
-   * レベル
-   */
-  protected abstract _lebel: number = 0;
-
-  /**
-   * おぼえているわざ
-   */
-  protected readonly abstract _moveList: Move[] = [];
-  
-  /**
-   * 種族値・個体値・努力値を合わせた決定ステータス
-   */
-  protected abstract _basicTotalStatus: TBasicStatus;
-
-  /**
    * ゲッター・セッター
    */
   get name() {
     return this._name;
   }
 
-  get lebel() {
-    return this._lebel;
-  }
-
-  set lebel(lebel) {
-    this._lebel = lebel;
-  }
-
-  get basicPokemonStatus() {
-    return this._basicPokemonStatus;
-  }
-
-  get basicEffortStatus() {
-    return this._basicEffortStatus;
-  }
-
-  set basicEffortStatus(basicEffortStatus) {
-    this._basicEffortStatus = basicEffortStatus;
-  }
-
-  get basicIndividualStatus() {
-    return this._basicIndividualStatus;
-  }
-
-  set basicIndividualStatus(basicIndividualStatus) {
-    this._basicIndividualStatus = basicIndividualStatus;
-  }
-
-  get basicTotalStatus() {
-    return this._basicTotalStatus;
-  }
-
-  set basicTotalStatus(basicTotalStatus) {
-    this._basicTotalStatus = basicTotalStatus;
-  }
-
   get groups() {
     return this._groups;
   }
 
-  get moveListToRequest() {
-    return this._moveListToRequest;
+  get pokemonType() {
+    return this._pokemonType;
   }
 
-
-  /**
-   * ステータスを計算し、_basicTotalStatusプロパティへ格納
-   * @param _basicPokemonStatus 種族値
-   * @param _basicIndividualStatus 個体値
-   * @param _basicEffortStatus 努力値
-   */
-  calculateBasicStatus(_basicPokemonStatus: TBasicStatus, _basicIndividualStatus: TBasicStatus, _basicEffortStatus: TBasicStatus): void {
-    for (let k of Object.keys(this._basicTotalStatus) as (keyof TBasicStatus)[]) {
-      const common = ((_basicPokemonStatus[k] * 2) + _basicIndividualStatus[k] + _basicEffortStatus[k]) * this._lebel / 100;
-      this._basicTotalStatus[k] = k === 'hp' 
-        ? Math.round(common + this._lebel + 10)
-        : Math.round(common + 5)
-      ;
-    }
+  get moveListToRequest() {
+    return this._moveListToRequest;
   }
 }
