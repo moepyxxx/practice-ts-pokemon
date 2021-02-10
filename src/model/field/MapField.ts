069f871a4ee49741f344593c3636723fb91a2321
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-02-10 09:34:00 +0900

    mapfieldのプロパティであるfieldを複数へ変更

--- src/model/field/MapField.ts
+++ src/model/field/MapField.ts
@@ -6,16 +6,48 @@ export class MapField {
   /**
    * フィールド名
    */
- _name: string;
+ private readonly _name: string;
 
   /**
    * フィールドを構成する場所要素
    */
- _place: Place[];
+  private readonly _place: Place[];
+
+  /**
+  * 近くのフィールド
+  */
+ private _nearField: MapField[];
 
  /**
   * 登場する人物
   */
- _humans?: (Ordinary | Trainer)[];
+ private readonly _humans?: (Ordinary | Trainer)[];
+
+ constructor(name: string, place: Place[], nearField?: MapField[], humans?: (Ordinary | Trainer)[]) {
+   this._name = name;
+   this._place = place;
+   this._nearField = nearField ? this._nearField : [];
+   this._humans = humans? this._humans : [];
+ }
+
+ get name() {
+   return this._name;
+ }
+
+ get place() {
+   return this._place;
+ }
+
+ get nearField() {
+   return this._nearField;
+ }
+
+ set nearField(nearField) {
+   this._nearField = nearField;
+ }
+
+ get humans() {
+   return this._humans;
+ }
 
 }
