9983d1fea7b7d9c9a3f0547c3fdcf510da0e7228
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-02-17 23:01:12 +0900

    ポケモンセンターにジョーイさんと主人公からジョーイさんに回復依頼をするメソッド・ポケモンを回復するメソッドを追加

--- src/model/field/PokemonCener.ts
+++ src/model/field/PokemonCener.ts
@@ -5,14 +5,25 @@ import { OwnPokemon } from '../pokemon/OwnPokemon';
 
 import { IRecoverPokemons } from '../../utils/interface/IRecoverPokemons';
 import { RecoverMachine } from '../machine/RecoverMachine';
+import { Hero } from '../human/Hero';
 
 export class PokemonCener extends Place implements IRecoverPokemons {
+
+  /**
+   * ジョーイさんを追加
+   */
+  private joisan: Ordinary = new Ordinary('ジョーイさん', '女', 'ポケモンを回復することを伝える');
 
   constructor(name: string, humans: (Ordinary | Trainer)[]) {
     super(name, humans);
   }
 
-  recoverPokemons(...pokemons: OwnPokemon[]) :void {
+  talkJoisanToRecoverPokemon(...pokemons: OwnPokemon[]): void {
+    this.joisan.talk(this.joisan._talkKey);
+    this.recoverPokemons(...pokemons);
+  }
+
+  recoverPokemons(...pokemons: OwnPokemon[]): void {
     RecoverMachine.getInstance().recoverPokemon(...pokemons);
   }
   
